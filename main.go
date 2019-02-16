4faeb44542910aa6400be9399d356ff4421b7da9
Kazuki Sawada <kazuki@6715.jp>
2019-02-16 19:44:35 +0900

    fast

--- main.go
+++ main.go
@@ -1,31 +1,26 @@
 package main
 
 import (
+	"fmt"
+
 	"golang.org/x/net/websocket"
 )
 
 func main() {
 	var ans []byte
-	buffer := make([]byte, 2048)
+	buffer := make([]byte, 512)
 	sigStart := []byte("{\"signal\":\"start\"}")
-	ansPrefix := []byte("{\"answer\":\"")
-	ansSuffix := []byte("\"}")
+	countAns := []byte("{\"answer\":0}")
+	ansTemplate := append([]byte("{\"answer\":\""), make([]byte, 512)...)
 
 	ws, err := websocket.Dial("wss://saiyo2020.moi.st/websocket/ae5108bdb3dd4326a6a41693a4d7b0de", "", "https://saiyo2020.moi.st/")
 	if err != nil {
 		panic(err)
 	}
 
-	_, err = ws.Write(sigStart)
-	if err != nil {
-		panic(err)
-	}
-
+	ws.Write(sigStart)
 	for {
-		_, err = ws.Read(buffer)
-		if err != nil {
-			panic(err)
-		}
+		ws.Read(buffer)
 		//fmt.Println("<RECV>", string(buffer))
 
 		if buffer[16] == 117 {
@@ -33,7 +28,8 @@ func main() {
 			count = 48
 			status := 0
 
-			for i := 31; buffer[i] != 34; i++ {
+			i := 31
+			for ; buffer[i] != 34; i++ {
 				if buffer[i] == 109 {
 					status = 1
 				} else if status == 1 && buffer[i] == 111 {
@@ -46,89 +42,96 @@ func main() {
 				}
 			}
 
-			ans = append(make([]byte, 0, 64), ansPrefix...)
-			ans = append(ans, count)
-			ans = append(ans, ansSuffix...)
+			ans = countAns
+			countAns[10] = count
 		} else if buffer[16] == 112 {
-			text := make([]byte, 256)
+			ans = ansTemplate
+			pointer := 11
 			status := 0
 
-			i := 33
-			for ; buffer[i] != 34; i++ {
-				text[i-33] = buffer[i]
+			for i := 33; buffer[i] != 34; i++ {
+				ans[pointer] = buffer[i]
+				pointer++
+
 				if buffer[i] == 109 {
 					status = 1
 				} else if status == 1 && buffer[i] == 111 {
 					status = 2
 				} else if status == 2 && buffer[i] == 105 {
 					status = 0
-					text[i-35] = 77
-					text[i-34] = 79
-					text[i-33] = 73
+					ans[pointer-3] = 77
+					ans[pointer-2] = 79
+					ans[pointer-1] = 73
 				} else {
 					status = 0
 				}
 			}
 
-			ans = append(make([]byte, 0, 64), ansPrefix...)
-			ans = append(ans, text[:i-33]...)
-			ans = append(ans, ansSuffix...)
+			ans[pointer] = 34
+			ans[pointer+1] = 125
+			ans = ans[:pointer+2]
 		} else if buffer[16] == 118 {
-			text := make([]byte, 256)
+			ans = ansTemplate
+			pointer := 11
 			status := 0
 
 			i := 33
 			for ; buffer[i] != 34; i++ {
-				text[255-(i-33)] = buffer[i]
-				if buffer[i] == 109 {
+			}
+			i--
+
+			for ; buffer[i] != 34; i-- {
+				ans[pointer] = buffer[i]
+				pointer++
+
+				if buffer[i] == 105 {
 					status = 1
 				} else if status == 1 && buffer[i] == 111 {
 					status = 2
-				} else if status == 2 && buffer[i] == 105 {
+				} else if status == 2 && buffer[i] == 109 {
 					status = 0
-					text[255-(i-33)] = 109
-					text[255-(i-35)] = 105
+					ans[pointer-1] = 105
+					ans[pointer-3] = 109
 				} else {
 					status = 0
 				}
 			}
 
-			ans = append(make([]byte, 0, 64), ansPrefix...)
-			ans = append(ans, text[256-(i-33):]...)
-			ans = append(ans, ansSuffix...)
+			ans[pointer] = 34
+			ans[pointer+1] = 125
+			ans = ans[:pointer+2]
 		} else if buffer[16] == 116 {
-			text := make([]byte, 256)
-			pointer := 0
+			ans = ansTemplate
+			pointer := 11
 			counter := 0
 
 			buffer[26] = 0
 			buffer[28] = 0
 
-			i := 26
-			for ; buffer[i] != 34; i++ {
+			for i := 26; buffer[i] != 34; i++ {
 				if buffer[i+3] == 109 && buffer[i+4] == 111 && buffer[i+5] == 105 {
 					counter = 9
 				}
 				if counter > 0 {
 					if i >= 29 {
-						text[pointer] = buffer[i]
+						ans[pointer] = buffer[i]
 						pointer++
 					}
 					counter--
 				}
 			}
 
-			ans = append(make([]byte, 0, 64), ansPrefix...)
-			ans = append(ans, text[:pointer]...)
-			ans = append(ans, ansSuffix...)
+			ans[pointer] = 34
+			ans[pointer+1] = 125
+			ans = ans[:pointer+2]
 		} else {
-			panic(string(buffer))
+			ws.Close()
+			break
 		}
 
 		//fmt.Println("<SEND>", string(ans))
-		_, err = ws.Write(ans)
-		if err != nil {
-			panic(err)
-		}
+		ws.Write(ans)
 	}
+
+	fmt.Println("<STOP>", string(buffer))
 }
